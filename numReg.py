4ba97b51fab8001e45b46e2bcc46041185928caf
Leucist <vlad2ka@tut.by>
2021-08-03 01:53:31 +0300

    Update numReg.py

--- numReg.py
+++ numReg.py
@@ -1,8 +1,13 @@
 # Gonna try to make it as a console application first
 
+# CONSTANTS ----------------->
+# Indicates the length of the continuation of the sequence
+CONTINUATION = 5
+
 
 class Regularity:
     """Is used to create sequence and find regularities"""
+
     def __init__(self):
         """Creates new sequence"""
         self.reg = []
@@ -39,13 +44,33 @@ class Regularity:
             if result[1] == 0:
                 print("Арифметическая и геометрическая прогрессия с разностью 0 или знаменателем 1")
                 return 0
-            print("Это арифметическая прогрессия, разность - d=" + str(result[1]))
+            print("Это арифметическая прогрессия, разность - d = " + str(result[1]))
+            print("Последующие " + str(CONTINUATION) + " членов прогрессии: ..", end="")
+            for i in range(1, CONTINUATION + 1):
+                print(self.reg[-1] + result[1] * i, end=", ")
             return 0
         result = self.check_geometric()
         if result[0] == 0:
-            print("Это геометрическая прогрессия, знаменатель - r=" + str(result[1]))
+            print("Это геометрическая прогрессия, знаменатель - r = " + str(result[1]))
+            print("Последующие " + str(CONTINUATION) + " членов прогрессии: ..", end="")
+            for i in range(1, CONTINUATION + 1):
+                print(self.reg[-1] * result[1] ** i, end=", ")
             return 0
+        result = self.check_other()
+        if result[0] == 0:
+            if result[1] == 0:
+                print(
+                    "Закономерность найдена! Каждый новый член последовательности является суммой двух предыдущих и " +
+                    str(result[2]))
+                print("Последующие " + str(CONTINUATION) + " членов последовательности: ..", end="")
+                for i in range(1, CONTINUATION + 1):
+                    new_number = self.reg[-2] + self.reg[-1] + result[2]
+                    print(new_number, end=", ")
+                    self.reg.append(new_number)
+                return 0
+        print("Закономерности не найдено.")
 
+    # Последовательности НУЖНО прогнать потом, на случай ошибки. Кол-во чисел от начала можно указывать опционально
     def check_arithmetic(self):
         # find difference of potential progression
         d = self.reg[1] - self.reg[0]
@@ -53,16 +78,24 @@ class Regularity:
         if self.reg[1] + d == self.reg[2]:
             return 0, d
         else:
-            return 1, None  # follow pattern just in case, maybe it's redundant~
+            return 1, None
 
     def check_geometric(self):
         # find common ratio
-        r = self.reg[1] / self.reg[0]
+        r = self.reg[1] // self.reg[0]
         # check if it is a geometric progression by multiplying the second number by the common ratio
         if self.reg[1] * r == self.reg[2]:
             return 0, r
         else:
             return 1, None
+
+    def check_other(self):
+        s = self.reg[0] + self.reg[1]
+        for i in range(11):
+            if s + i == self.reg[2]:
+                return 0, 0, i
+        del s
+
 
 
 def start():
